c763713a64550974b485f046716dfe13c61be1a7
PUPILUPI <bev6889@gmail.com>
2024-04-21 18:40:23 +0300

    не рабочий код!!!

---
+++ src/main/java/ru/belov/radioComponentsService/RadioComponentsApplication.java
@@ -0,0 +1,12 @@
+package ru.belov.radioComponentsService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadioComponentsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RadioComponentsApplication.class, args);
+    }
+}
+
---
+++ src/main/java/ru/belov/radioComponentsService/db/entity/User.java
@@ -0,0 +1,31 @@
+package ru.belov.radioComponentsService.db.entity;
+
+import jakarta.persistence.*;
+
+//@Entity
+//@Table(name = "users")
+public class User {
+//        @Id
+//        @GeneratedValue(strategy = GenerationType.IDENTITY)
+//        @Column(name = "id")
+//        private Long id;
+//
+//        @Column(name = "username", unique = true, nullable = false)
+//        private String userName;
+//
+//        @Column(name = "password", nullable = false)
+//        private String password;
+//
+//        @Column(name = "first_name")
+//        private String firstName;
+//
+//        @Column(name = "last_name")
+//        private String lastName;
+//
+//        @Column(name = "email")
+//        private String email;
+//
+//        // Далее конструктор по умолчанию, геттеры и сеттеры
+//        // Или можете использовать Lombok
+
+}
